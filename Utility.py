53d18585c345e4a3516f855a8a9fee8d709a1acc
tianzhiliang <tianzhiliang@baidu.com>
2016-04-16 22:34:43 +0800

    add method to utility

--- Utility.py
+++ Utility.py
@@ -1,7 +1,44 @@
+#coding:utf8
 from __future__ import print_function
+from collections import Counter
+import itertools
 import numpy as np
 import random
 import os
+import re
+
+"""
+Original taken from https://github.com/dennybritz/cnn-text-classification-tf
+"""
+
+def clean_str(string, TREC=False):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Every dataset is lower cased except for TREC
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)     
+    string = re.sub(r"\'s", " \'s", string) 
+    string = re.sub(r"\'ve", " \'ve", string) 
+    string = re.sub(r"n\'t", " n\'t", string) 
+    string = re.sub(r"\'re", " \'re", string) 
+    string = re.sub(r"\'d", " \'d", string) 
+    string = re.sub(r"\'ll", " \'ll", string) 
+    string = re.sub(r",", " , ", string) 
+    string = re.sub(r"!", " ! ", string) 
+    string = re.sub(r"\(", " \( ", string) 
+    string = re.sub(r"\)", " \) ", string) 
+    string = re.sub(r"\?", " \? ", string) 
+    string = re.sub(r"\s{2,}", " ", string)    
+    return string.strip() if TREC else string.strip().lower()
+
+def clean_str_sst(string):
+    """
+    Tokenization/string cleaning for the SST dataset
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)   
+    string = re.sub(r"\s{2,}", " ", string)    
+    return string.strip().lower()
+
 
 class StreamDataGenerator(object):
 	def __init__(self, path, batch, validation = 0.1, seed = 9527):
@@ -35,7 +72,7 @@ class StreamDataGenerator(object):
 	def eof(self):
 		return self.fin.tell() == os.fstat(self.fin.fileno()).st_size
 
-def SelectMaximumProbability(mat):
+def selectMaximumProbability(mat):
 	row, col = mat.shape
 	m = mat.max(axis = 1)
 	indices = mat == np.dot(m.reshape((row, 1)), np.ones((1, col)))
@@ -43,11 +80,42 @@ def SelectMaximumProbability(mat):
 	response[indices] = 1.0
 	return response
 
+def buildVocab(sentences, padding = False):
+    '''
+    Builds a vocabulary mapping from word to index based on the sentences.
+    Returns vocabulary mapping and inverse vocabulary mapping.
+    '''
+    # Build vocabulary
+    word_counts = Counter(itertools.chain(*sentences))
+    # Mapping from index to word
+    vocabulary = [x[0] for x in word_counts.most_common()]
+    # Mapping from word to index
+    if padding:
+        word2index = {x: i + 1 for i, x in enumerate(vocabulary)}
+    else:
+        word2index = {x: i for i, x in enumerate(vocabulary)}
+    return [vocabulary, word2index]
+
+def load_word2vec(coll, vocabulary):
+    import pymongo
+    '''
+    Load word2vec from local mongodb
+    '''
+    word2vec = {}
+    for word in vocabulary:
+        response = coll.find_one({'word':word})
+        if response:
+            word2vec[word] = np.array(response['vector'])
+    return word2vec
+
+def embedding_layer_weights(vocabulary, word2vec, dim = 300):
+    embedding_weights = np.array([word2vec.get(w, np.random.uniform(-0.25,0.25,dim)) for w in vocabulary])
+    return [embedding_weights]
+
 if __name__ == '__main__':
-	a = np.array([[1,2,3],[3,1,2]])
-	print(SelectMaximumProbability(a))
-	sdg = StreamDataGenerator("Utility.py", 5, 0.2)
-	sdg.processor(lambda x: (0,0))
-	gen = sdg.generate()
-	for _ in gen:
-		pass
+    client = pymongo.MongoClient()
+    coll = client['word2vec']['en_google']
+    vocab, w2i = buildVocab([['hello', 'world'],['hello', 'python']])
+    print(vocab, w2i)
+    word2vec = load_word2vec(coll, vocab)
+    print(word2vec)
